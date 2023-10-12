0f7866bae7a3ff860d89637895c197859f51d017
infinite <2062468100@qq,com>
2023-10-12 10:12:51 +0800

    2 ...

--- gee/router.go
+++ gee/router.go
@@ -1,31 +1,84 @@
 package gee
 
 import (
-	"fmt"
-	"log"
+	"strings"
 )
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
 	return &router{
+		roots:    make(map[string]*node),
 		handlers: make(map[string]HandlerFunc),
 	}
 }
 
+func (r *router) parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+
+	for _, part := range vs {
+		if part != "" {
+			parts = append(parts, part)
+			if part[0] == '*' {
+				break
+			}
+		}
+	}
+
+	return parts
+}
+
 func (r *router) addRoute(method string, path string, handler HandlerFunc) {
-	log.Printf("Route %s - %s", method, path)
+	parts := r.parsePattern(path)
+
 	key := method + "-" + path
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(path, parts, 0)
 	r.handlers[key] = handler
 }
 
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := r.parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := r.parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
+}
+
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
 	} else {
-		fmt.Fprintf(c.Writer, "404 NOT FOUND %q", c.Path)
+		c.String(404, "404 NOT FOUND:%s", c.Path)
 	}
 }
--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 
 func main() {
 	r := gee.New()
-	r.Get("/", func(c *gee.Context) {
-		c.HTML(200, "<h1>hello</h1>")
+	//r.Get("/", func(c *gee.Context) {
+	//	c.HTML(200, "<h1>hello</h1>")
+	//})
+	//
+	//r.Get("/hello", func(c *gee.Context) {
+	//	c.String(200, "hello %s,you are at %s", c.Query("name"), c.Path)
+	//})
+	//
+	//r.POST("/login", func(c *gee.Context) {
+	//	c.JSON(200, gee.H{
+	//		"username": c.PostForm("username"),
+	//		"password": c.PostForm("password"),
+	//	})
+	//})
+
+	r.Get("/hello/:name", func(c *gee.Context) {
+		c.String(200, "hello %s,you are at %s", c.Param("name"), c.Path)
 	})
 
-	r.Get("/hello", func(c *gee.Context) {
-		c.String(200, "hello %s,you are at %s", c.Query("name"), c.Path)
-	})
-
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(200, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
-	})
 	r.Run(":8080")
 }
