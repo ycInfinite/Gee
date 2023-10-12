05441bd0a1a704228e5c08ea8ea0898a3f8ccd68
infinite <2062468100@qq,com>
2023-10-12 19:07:16 +0800

    3 ...

--- gee/gee.go
+++ gee/gee.go
@@ -7,11 +7,50 @@ import (
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+
+	newgroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newgroup)
+	return newgroup
+}
+
+func (group *RouterGroup) addRoute(method string, pattern string, handler HandlerFunc) {
+	path := group.prefix + pattern
+
+	group.engine.router.addRoute(method, path, handler)
+}
+
+func (group *RouterGroup) Get(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) addRoute(pattern string, method string, handler HandlerFunc) {
--- main.go
+++ main.go
@@ -21,8 +21,28 @@ func main() {
 	//	})
 	//})
 
+	v1 := r.Group("/v1")
+	{
+		v1.Get("/hello", func(c *gee.Context) {
+			c.String(200, "hello %s,you are at %s", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("v2")
+	{
+		v2.Get("/thank", func(c *gee.Context) {
+			c.String(200, "thank %s,you are at %s", c.Query("name"), c.Path)
+		})
+	}
+
 	r.Get("/hello/:name", func(c *gee.Context) {
 		c.String(200, "hello %s,you are at %s", c.Param("name"), c.Path)
+	})
+
+	r.Get("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(200, gee.H{
+			"filepath": c.Params["filepath"],
+		})
 	})
 
 	r.Run(":8080")
